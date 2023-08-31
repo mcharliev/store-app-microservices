a494353577f710d4adb1ba5d84cbbc009065a0c3
Merdan Charliev <mcharliev@gmail.com>
2023-08-31 16:00:02 +0300

    добавил сервисы: продуктов, заказов и склада

---
+++ order-service/src/main/java/com/programmingtechie/order/service/model/Order.java
@@ -0,0 +1,22 @@
+package com.programmingtechie.order.service.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name = "t_orders")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String orderNumber;
+    @OneToMany(cascade = CascadeType.ALL,fetch = FetchType.LAZY)
+    private List<OrderLineItems> orderLineItemsList;
+}

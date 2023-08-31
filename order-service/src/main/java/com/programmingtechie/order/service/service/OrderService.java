9c1d9ed33a23b7203bd5380e2ec3bac0e70f90b8
Merdan Charliev <mcharliev@gmail.com>
2023-08-31 22:34:36 +0300

    добавил и настроил  eureka server

--- order-service/src/main/java/com/programmingtechie/order/service/service/OrderService.java
+++ order-service/src/main/java/com/programmingtechie/order/service/service/OrderService.java
@@ -20,7 +20,7 @@ import java.util.UUID;
 @Transactional
 public class OrderService {
     private final OrderRepository orderRepository;
-    private final WebClient webClient;
+    private final WebClient.Builder webClientBuilder;
 
     public void placeOrder(OrderRequest orderRequest) {
         Order order = new Order();
@@ -37,8 +37,8 @@ public class OrderService {
                 .toList();
 
         //Call to Inventory Service, and place order if product is in stock
-        InventoryResponse[] inventoryResponsesArray = webClient.get()
-                .uri("http://localhost:8082/api/inventory",
+        InventoryResponse[] inventoryResponsesArray = webClientBuilder.build().get()
+                .uri("http://inventory-service/api/inventory",
                         uriBuilder -> uriBuilder.queryParam("skuCode", skuCodes).build())
                 .retrieve()
                 .bodyToMono(InventoryResponse[].class)
